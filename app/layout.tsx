0ea8a0c7cfa6f1c1e83c7013bc9f623c4e60480c
fhub-1 <flutterhub1.21.0@gmail.com>
2025-02-27 08:11:20 +0300

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
+        <SpeedInsights/>
         {children}
       </body>
     </html>
