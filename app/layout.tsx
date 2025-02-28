1618a883fe9eb5684e2a7070d49ea2efee0f0889
fhub-1 <flutterhub1.21.0@gmail.com>
2025-02-28 13:06:54 +0700

    Import and use the <Analytics/> React component into your app's layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -46,6 +47,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         <SpeedInsights/>
+        <Analytics />
         {children}
       </body>
     </html>
